866320ddd43bc074818585c958de3988401cecca
leonardork <leodegolim@yahoo.com.br>
2018-01-02 03:13:00 -0200

    tamanhos do quebra cabeça definido

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
+function criaCarta(imgSrc, backImg, lado, tamanhoX, tamanhoY) {
+	var imageFrente = render(imgSrc, tamanhoX , tamanhoY);
+  var imageBack = render(backImg, tamanhoX, tamanhoY);
+	var carta = {
+		frente: imageFrente,
+		fundo: imageBack,
+		lado: lado
+	}
 
-		
+	return carta;
+}
+
+function render(imgSrc, tamanhoX, tamanhoY) {
+
+	
+	var image = new Image();
+	image.height = tamanhoY;
+	image.width = tamanhoX;
+	
+	image.src = imgSrc;
+
+	return image;
+}
